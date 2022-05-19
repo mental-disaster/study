848460b7ec545b09c4b3a6534fc9d5ccbbf45cdc
GyeongHun <54014203+mental-disaster@users.noreply.github.com>
2022-05-19 17:39:20 +0900

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -14,3 +14,27 @@ def BFS(graph,start):
             for new in linked:
                 q.append(new[new.index(node)^1])
     return visit
+
+import copy
+
+def solution(n, edge):
+    def BFS(graph,start):
+        visit = []
+        hierarchy = []
+        way = [start]
+        q = []
+
+        while(len(visit)<n):
+            hierarchy.append(list(set(way).difference(visit)))
+            q = copy.deepcopy(hierarchy[-1])
+            run = []
+            while(q):
+                node = q.pop(0)
+                if(node not in visit):
+                    visit.append(node)
+                    link = [i for i in graph if(node in i)]
+                    for new in link:
+                        way.append(new[new.index(node)^1])
+        return hierarchy
+    
+    return len(BFS(edge,1)[-1])
